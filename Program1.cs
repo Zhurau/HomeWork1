dac27ccb28785ddc6a63dde5b79b095eca86bfa5
Zhurau <53076666+Zhurau@users.noreply.github.com>
2021-10-10 17:09:40 +0300

    Task1

---
+++ Program1.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Task1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            bool isRight = true;
+
+            byte age = 24;
+            sbyte degreeC = -5;
+            short metersDeepHight = -2345;
+            ushort memoryAddress = 45933;
+            int number = -80500;
+            uint populationCountry = 150000000;
+            long numberBig = -50100200300;
+            ulong populationWhole = 7850450500;
+
+            float percentage = 45.4f;
+            double PI = 3.14159265359;
+            decimal financialSumm = 175500144522.25m;
+
+            char symbol = 'S';
+            string sentence = "It is string.";
+            object emptyObject = null;
+        }
+    }
+}
